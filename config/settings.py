a38abfecba4838b89f919040fc72b46ea7219542
anita564-lucy <lucyanita564@gmail.com>
2026-04-24 12:18:17 +0300

    Remove dj_database_url

--- config/settings.py
+++ config/settings.py
@@ -61,11 +61,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
-import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
